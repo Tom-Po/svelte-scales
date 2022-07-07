106c3f063f27cff3cb47e07f0a8ce45c55b088f6
Tom Poidevin <tom.poidevin@gmail.com>
2022-07-08 00:50:41 +0200

    root identifier

--- src/lib/Neck.svelte
+++ src/lib/Neck.svelte
@@ -1,8 +1,20 @@
 <script>
+  import { neck } from '../stores.js'
+
   export let chord = {
+    chordName: 'Em',
     strings: '0,2,2,0,0,0',
   }
+
+  let showNotes = false
   const strings = chord.strings.split(',')
+
+  let neckNotes = []
+  let rootNote = 'C'
+
+  neck.subscribe(notes => {
+    neckNotes = notes
+  })
 
   const getRange = (arr) => {
     const nums = []
@@ -37,40 +49,88 @@
 <div class='neck'>
   <h1>Neck</h1>
   <h2>lowest {range.min} | highest: {range.max}</h2>
-  <div class='neck-template' style='--height: {range.max - range.min}'>
-    {#each notes as string}
+  <h3>Showing : {showNotes ? 'Notes' : 'Fingering'}</h3>
+  <button on:click={()=> showNotes = !showNotes}>Show notes</button>
+  <div class='neck-template'
+       style='--height: {range.max - range.min + 1}; --neck-border: {range.min === 0 ? "5px solid" : ""}'>
+    {#each notes as string, i}
       {#if string.isMuted}
-        <div class='string flex-center' style='--top-factor: 0'>X</div>
+        <div class='string muted flex-center' style='--top-factor: 0'>X</div>
       {:else}
-        <div class='string flex-center' style='--top-factor: {string.noteNumber - range.min}'>{string.noteNumber}</div>
+        {#if showNotes}
+          <div
+            class={neckNotes[i][string.noteNumber] === rootNote ? 'string flex-center is-root' : 'string flex-center' }
+            style='--top-factor: {string.noteNumber - range.min}'>
+            {neckNotes[i][string.noteNumber]}
+          </div>
+        {:else}
+          <div class='string flex-center' style='--top-factor: {string.noteNumber - range.min}'>
+            {string.noteNumber}
+          </div>
+        {/if}
       {/if}
+    {/each}
+    {#each notes as _, i}
+      <div class='string-wave' style='--left-factor: {i}'></div>
+    {/each}
+    {#each Array(range.max - range.min + 1) as _, i}
+      <div class='fret'></div>
     {/each}
   </div>
 </div>
 
 <style>
   .neck-template {
-    --width: 300px;
-    position: relative;
+    --width: 33vw;
+    --string-width: calc(var(--width) / 6);
+    --neck-border: 0;
+
+    border-top: var(--neck-border);
     background-color: var(--bg-color);
+
+    position: relative;
+
     width: var(--width);
-    height: calc(var(--height) * calc(var(--width) / 3));
+    height: calc(var(--height) * var(--string-width));
+
     margin: 0 auto;
   }
 
+  .string-wave {
+    --half-string-wave: calc(var(--string-width) / 2 - 2px);
+    --left: calc(var(--left-factor) * var(--string-width) + var(--half-string-wave));
+    background-color: blue;
+
+    position: absolute;
+    bottom: 0;
+    top: 0;
+    left: var(--left);
+    width: 4px;
+  }
+
   .string {
+    --top: calc(var(--top-factor) * var(--string-width));
     cursor: pointer;
     position: absolute;
-    top: calc(var(--top-factor) * calc(var(--width) / 6));
-    width: calc(var(--width) / 6);
-    height: calc(var(--width) / 6);
+    top: var(--top);
+    width: var(--string-width);
+    height: var(--string-width);
     background-color: red;
     border-radius: 50%;
-    /*box-shadow: #DAFDBA;*/
+    transform: translateY(-1px) scale(.9);
+    transform-origin: center center;
+    z-index: 10;
+  }
+
+  .string.is-root {
+    background-color: green;
+  }
+  .string.muted {
+    background-color: gray;
   }
 
   .string:nth-of-type(2) {
-    left: calc(var(--width) / 6);
+    left: var(--string-width);
   }
 
   .string:nth-of-type(3) {
@@ -87,5 +147,17 @@
 
   .string:nth-of-type(6) {
     left: calc(var(--width) / 6 * 5);
+  }
+
+  .fret {
+    border-bottom: 2px solid grey;
+    height: calc(var(--string-width) - 2px);
+    width: 100%;
+  }
+
+  @media screen and (max-width: 590px) {
+    .neck-template {
+      --width: 300px;
+    }
   }
 </style>
--- src/stores.js
+++ src/stores.js
@@ -1,4 +1,4 @@
-import {writable} from 'svelte/store'
+import { writable } from 'svelte/store'
 
 export const majorRootNotes = writable(['A', 'E', 'B', 'Gb/F#', 'Db', 'Ab', 'Eb', 'Bb', 'F', 'C', 'G', 'D'])
 export const minorRootNotes = writable(['F#', 'C#', 'G#', 'Eb/D#', 'Bb', 'F', 'C', 'G', 'D', 'A', 'E', 'B'])
@@ -12,4 +12,22 @@ export const chords = writable([
     chordName: 'Cmaj7',
     strings: 'X 3 5 4 5 X',
   },
+])
+
+
+// TODO, optimize offset
+const eLowString = ['E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E']
+const aString = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'E', 'F', 'F#', 'G', 'G#', 'A']
+const dString = ['D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D']
+const gString = ['G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G']
+const bString = ['B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+const eString = eLowString
+
+export const neck = writable([
+  eString,
+  aString,
+  dString,
+  gString,
+  bString,
+  eString
 ])
